67284ed10b4b32baf01acf0f1bfa3be5604a09fd
edo01 <edocarra012@gmail.com>
2018-11-29 23:28:27 +0100

    changing qrcode activity.

--- application/app/src/main/java/org/altervista/edoardo/bfconnect/activities/QrCode.java
+++ application/app/src/main/java/org/altervista/edoardo/bfconnect/activities/QrCode.java
@@ -1,7 +1,12 @@
 package org.altervista.edoardo.bfconnect.activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
+import android.view.WindowManager;
 
 import org.altervista.edoardo.bfconnect.R;
 
@@ -10,7 +15,22 @@ public class QrCode extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_qr_code);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.ic_bf_connect_horizontal_white);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.three_dots,menu);
+        return true;
     }
 
 
