c82712090c095e00316c99940f3791779f7532cc
FrancescoTaddia <fctaddia34@gmail.com>
2018-12-04 10:53:26 +0100

    Merging

--- application/app/src/main/java/org/iisbelluzzifioravanti/app/bfconnect/activities/threeDots/HQRuno.java
+++ application/app/src/main/java/org/iisbelluzzifioravanti/app/bfconnect/activities/threeDots/HQRuno.java
@@ -1,34 +1,25 @@
 package org.iisbelluzzifioravanti.app.bfconnect.activities.threeDots;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 
 import org.iisbelluzzifioravanti.app.bfconnect.BaseActivity;
 import org.iisbelluzzifioravanti.app.bfconnect.R;
 
-public class Hrileva extends BaseActivity {
+public class HQRuno extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_hrileva);
-    }
+        setContentView(R.layout.activity_hqruno);
 
-    @Override
-    public void activityPage() {
-
-    }
-
-    @Override
-    public int getContentViewId() {
-        return 0;
-    }
-
-    @Override
-    public int getNavigationMenuItemId() {
-        return 0;
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.ic_bf_connect_horizontal_white);
     }
 }
