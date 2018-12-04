d1c49b15ebd60dccc466f6d0f380c2b038d597f0
FrancescoTaddia <fctaddia34@gmail.com>
2018-12-05 00:02:17 +0100

    Addition activity_hnfcdue but is still in progress.

---
+++ application/app/src/main/java/org/iisbelluzzifioravanti/app/bfconnect/activities/threeDots/helpActivity/HNFCdue.java
@@ -0,0 +1,24 @@
+package org.iisbelluzzifioravanti.app.bfconnect.activities.threeDots.helpActivity;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import org.iisbelluzzifioravanti.app.bfconnect.R;
+
+public class HNFCdue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_hnfcdue);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.ic_bf_connect_horizontal_white);
+    }
+}
