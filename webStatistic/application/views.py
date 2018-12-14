4f90007377c0bf7fce432ab9e4f0958af16c75b0
edo01 <edocarra012@gmail.com>
2018-12-14 12:42:51 +0100

    webstatistic

--- webStatistic/application/views.py
+++ webStatistic/application/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from .models import Statistic
 from .updater import Updater
 
-up = Updater('2', 30)
+up = Updater('2', 60)
 
 def showDashboard(request):
     if(not up.is_alive()):
         print('aggiornamento non in corso, attivazione:')
         up.start()
-    newStatistic = Statistic.objects.order_by('-created_date')[0] # taking the lst statistic in db
+    newStatistic = Statistic.objects.order_by('-created_date')[0] # taking the list statistic in db
     return render(request, 'application/statistic.html', {'statistic': newStatistic, 'label': Updater.label, 'errors':
                                                           Updater.errors, 'responses': Updater.responses})
