678f7b017bc3f8fcba6abcb83e76dda33c5a9d2f
edo01 <edocarra012@gmail.com>
2018-12-27 21:28:37 +0100

    added two activities PROGETTI.

---
+++ application/app/src/main/java/org/iisbelluzzifioravanti/app/bfconnect/activities/ProgettiTecnico.java
@@ -0,0 +1,15 @@
+package org.iisbelluzzifioravanti.app.bfconnect.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.iisbelluzzifioravanti.app.bfconnect.R;
+
+public class ProgettiTecnico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progetti_tecnico);
+    }
+}
