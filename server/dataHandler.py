41b738909933be5b7c9859e34005acbe93965ff1
edo01 <edocarra012@gmail.com>
2019-01-17 22:11:22 +0100

    uploading new photos.

--- server/dataHandler.py
+++ server/dataHandler.py
@@ -8,20 +8,22 @@ import json
 class DataHandler:
     time = 0
     # the number of nuumbers in rooms is variable
-    statistic = '{"middleTimeOfResponse":0,"responses":0,"errors":0,"rooms":[0,0,0,0,0,0,0,0,0,0,0,0],' \
-                '"pdf":[0,0,0,0,0,0,0,0,0,0]}'
+    statistic = '{"middleTimeOfResponse":0,"responses":0,"errors":0,"rooms":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],' \
+                '"pdf":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}'
     dictionary = json.loads(statistic)
 
     datasPath = 'data/'
     pdf = {
         '4': 'elettronica.pdf', '1': 'informatica.pdf', '3': 'chimica.pdf', '2': 'meccanica.pdf',
         '5': 'serale.pdf', '6': 'qualifiche.pdf','8': 'apparati.pdf', '7': 'manutenzione.pdf', '9': 'mezzi.pdf', '10':
-        'alternanza.pdf'
+        'alternanza.pdf', '11': 'mast.pdf', '12':'ducati.pdf','13':'opus.pdf','14':'magneti.pdf', '15':'filosofia.pdf',
+        '16': 'frigotronica.pdf', '17': 'stem.pdf', '18': 'texa.pdf', '19': 'toyota.pdf'
     }
 # the id must be of 6 chars
     room = {
-        'beOPEN': '0', 'ETC34c': '1', 'ETC620': '2', 'ETC34d': '3', 'CHT32c': '4', 'CHT645': '5', 'CHT3c6': '6',
-        'CHTT00': '7', 'EXt6po': '8', 'EXt400': '9' , 'EXt120': '10', 'EXt600': '11'
+        'beOPEN': '0', 'ETC34c': '1', 'ETC620': '2', 'ETC34d': '3', 'CHT32c': '4', 'CHT645': '5','CHTT00': '7',
+        'EXt6po': '8', 'EXt400': '9', 'EXt120': '10', 'EXt600': '11', 'IIc1tf':'12','IIc64x':'13',
+        'IIcpo0': '14','FFPP12': '15','TTm65h': '16', 'TTms10': '17', 'TTmqw0': '18', 'TTm111': '19'
     }
 
     # all the files with the JSON datas must be called 'roomContent.txt'
