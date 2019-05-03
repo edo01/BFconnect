6bae837dfa8d69d7ee05f58218c9d9798b30d194
FrancescoTaddia <fctaddia34@gmail.com>
2019-05-03 22:36:43 +0200

    Fixed BFconnect - Dashboard

---
+++ application/app/src/main/java/org/iisbelluzzifioravanti/app/bfconnect/activities/threeDots/AppInfo.java
@@ -0,0 +1,13 @@
+package org.iisbelluzzifioravanti.app.bfconnect.activities.threeDots;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_info);
+    }
+}
